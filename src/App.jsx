22d856da7d28c6ca08d11b0bb00703b42bc952bc
Amirhossein <amirsalehi248@yahoo.com>
2022-12-24 08:05:50 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Layout } from 'antd'
 import { BrowserRouter as Router} from 'react-router-dom'
 import './App.css'
 import { AppFooter, AppHeader, PageContent } from './components'
+const { Header, Footer, Sider, Content } = Layout;
 const App = () => {
 
   
@@ -9,7 +10,7 @@ const App = () => {
     <div className="App" dir='rtl'>
       <Router>
         <Layout>
-          <AppHeader />
+          <Header><AppHeader /></Header>
           <PageContent />
           <AppFooter />
         </Layout>
